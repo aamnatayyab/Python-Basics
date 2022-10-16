8f1f13ef5489d27cc6487a79953a6394bd374124
uoxp5r <>
2022-10-16 02:31:19 +0200

    key-takeaways from lab2

---
+++ lab2.py
@@ -0,0 +1,29 @@
+# KEY TAKE-AWAYS
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.") #The itsy bitsy spider climbed up the waterspout.
+print("\"") # escape character
+# My_name_is*Monty*Python.*
+print("My", "name", "is", sep="_", end="*") 
+print("Monty", "Python.", sep="*", end="*\n") 
+print(" *\n * *\n * *\n * *\n*** ***\n * *\n * *\n *****")
+
+print("2"*2) # 22
+print(2*2) # 4
+
+print(0.0000000000000000000001) # 1e-22
+
+print(0o123) # 83
+
+print(0x123) # 291
+
+print(-6 // 3) # integer division
+
+# 1. Legal function name: 
+#     Use a lowercase word or words. Separate words by underscores to improve readability.
+#     Two functions can not have same names.
+# 2. Python requires that there cannot be more than one instruction in a line.
+# 3. Note: Python makes one exception to this rule - it allows one instruction to spread across more than one line (which may be
+#    helpful when your code contains complex constructions).
+# 4. Print() function contains newline
+# 5. keywords: end="", sep=""
+# 6. string can be multiplied with integer
+# 7. operators precedence: PEMDAS
